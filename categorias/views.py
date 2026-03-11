dddc19f472e52188f89911d7af3b00681ca13f18
carolinamoralese <caromorales1396@gmail.com>
2026-03-11 16:54:58 -0500

    feat: se modifica categoria

--- categorias/views.py
+++ categorias/views.py
@@ -6,6 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import *
 from http import HTTPStatus
+from django.utils.text import slugify
 
 # Create your views here.
 class Clase1(APIView):
@@ -37,5 +38,16 @@ class Clase2(APIView):
         try:
             data= Categoria.objects.filter(pk=id).get()
             return JsonResponse({"data": {"id": data.id, "nombre":data.nombre, "slug": data.slug}}, status=HTTPStatus.OK)
+        except Categoria.DoesNotExist:
+            raise Http404
+
+
+    def put(self, request, id):
+        if request.data.get("nombre")== None or not request.data["nombre"]:
+            return JsonResponse({"estado": "error", "mensaje": "El campo nombre es obligatorio"}, status= HTTPStatus.BAD_REQUEST)
+        try:
+            data = Categoria.objects.filter(pk=id).get()
+            Categoria.objects.filter(pk=id).update(nombre=request.data.get("nombre"), slug=slugify(request.data.get("nombre")))
+            return JsonResponse({"estado":"ok", "mensaje": "Se modifica el registro exitosamente"}, status= HTTPStatus.ok)
         except Categoria.DoesNotExist:
             raise Http404
