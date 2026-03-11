61b325c9662b3fa2fe278a2824b6409f28e643d2
carolinamoralese <caromorales1396@gmail.com>
2026-03-10 21:20:53 -0500

    feat:se crea categoria

--- categorias/views.py
+++ categorias/views.py
@@ -9,13 +9,23 @@ from http import HTTPStatus
 
 # Create your views here.
 class Clase1(APIView):
+
+
     def get(self, request):
         #select * from irder by id desc
         data = Categoria.objects.order_by('-id').all() #nombre del modelo.objects.order_by('-id')
         datos_json = CategoriaSerializer(data, many=True)
         #return Response(datos_json.data)
         return JsonResponse({"data": datos_json.data},status=HTTPStatus.OK)
-        
+    
+
+    def post(self, request):
+        try:
+            Categoria.objects.create(nombre=request.data['nombre'])
+            return JsonResponse({"estado":"ok", "mensaje": "Se crea el registro exitosamente"}, status= HTTPStatus.CREATED)
+        except Exception as e:
+            raise Http404
+
 
 class Clase2(APIView):
 
