f0029d6a83da75cdc62adf0209bccf3115bb7f2a
carolinamoralese <caromorales1396@gmail.com>
2026-03-12 11:53:51 -0500

    feat:formatear fecha

--- recetas/serializers.py
+++ recetas/serializers.py
@@ -4,7 +4,11 @@ from .models import *
 # deberia tener nombre del modelo y sunfijo serializer
 class RecetaSerializer(serializers.ModelSerializer):
 
+    categoria = serializers.ReadOnlyField(source="categoria.nombre")# readonlyfiled pone el nombre de la categoria en lugar del id
+    #categoria = serializers.ChartField(source="categoria.nombre")
+    fecha = serializers.DateTimeField(format="%d/%m/%y")
+
     class Meta:
         model = Receta
-        #fields = ("id", "nombre", "slug")
-        fields= '__all__'
+        fields = ("id", "nombre", "slug", "tiempo", "descripcion", "fecha", "categoria", "categoria_id")
+        #fields= '__all__'
