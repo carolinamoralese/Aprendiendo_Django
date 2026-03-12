be2a7f6ec3a97386c08e04f993b26a9dce3a8a11
carolinamoralese <caromorales1396@gmail.com>
2026-03-12 12:35:22 -0500

    feat:end point para mostrar receta por id

--- recetas/views.py
+++ recetas/views.py
@@ -5,6 +5,10 @@ from http import HTTPStatus
 from django.utils.text import slugify
 from .serializers import *
 from .models import *
+from django.utils.dateformat import DateFormat
+from dotenv import load_dotenv
+import os
+
 
 # Create your views here.
 class Clase1(APIView):
@@ -14,3 +18,14 @@ class Clase1(APIView):
         data = Receta.objects.order_by('-id').all() #nombre del modelo.objects.order_by('-id')
         datos_json = RecetaSerializer(data, many=True)
         return JsonResponse({"data": datos_json.data},status=HTTPStatus.OK)
+    
+
+class Clase2(APIView):
+
+
+    def get(self, request, id):
+        try:
+            data = Receta.objects.filter(pk=id).get()
+            return JsonResponse({"data" :{ "id": data.id, "nombre": data.nombre, "slug": data.slug, "tiempo": data.tiempo, "descripcion":data.descripcion, "fecha": DateFormat(data.fecha).format('d/m/Y'), "categoria_id": data.categoria_id, "categoria": data.categoria.nombre, "imagen": f"{os.getenv('BASE_URL')}uploads/recetas/{data.foto}"}}, status= HTTPStatus.OK)
+        except Receta.DoesNotExist:
+            return JsonResponse({"estado": "error", "mensaje":"Recurso no disponible"}, status=HTTPStatus.NOT_FOUND)
