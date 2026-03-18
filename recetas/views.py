485030ddfb963a3b65b0c10c7187001b0bef0dd9
carolinamoralese <caromorales1396@gmail.com>
2026-03-18 18:21:35 -0500

    feat:modificar receta

--- recetas/views.py
+++ recetas/views.py
@@ -87,3 +87,39 @@ class Clase2(APIView):
             return JsonResponse({"data" :{ "id": data.id, "nombre": data.nombre, "slug": data.slug, "tiempo": data.tiempo, "descripcion":data.descripcion, "fecha": DateFormat(data.fecha).format('d/m/Y'), "categoria_id": data.categoria_id, "categoria": data.categoria.nombre, "imagen": f"{os.getenv('BASE_URL')}uploads/recetas/{data.foto}"}}, status= HTTPStatus.OK)
         except Receta.DoesNotExist:
             return JsonResponse({"estado": "error", "mensaje":"Recurso no disponible"}, status=HTTPStatus.NOT_FOUND)
+        
+    def put(self, request, id):
+        try:
+            data = Receta.objects.filter(pk=id).get()
+        except Receta.DoesNotExist:
+            return JsonResponse({"estado": "error", "mensaje":"Recurso no disponible"}, status=HTTPStatus.NOT_FOUND)
+        
+        if not request.data.get('nombre'):
+            return JsonResponse({"estado":"error", "mensaje":"El campo nombre es obligatorio"}, status=HTTPStatus.BAD_REQUEST)
+
+        if not request.data.get('tiempo'):
+            return JsonResponse({"estado":"error", "mensaje":"El campo tiempo es obligatorio"}, status=HTTPStatus.BAD_REQUEST)
+
+        if not request.data.get('descripcion'):
+            return JsonResponse({"estado":"error", "mensaje":"El campo descripcion es obligatorio"}, status=HTTPStatus.BAD_REQUEST)
+
+        if not request.data.get('categoria'):
+            return JsonResponse({"estado":"error", "mensaje":"El campo categoria es obligatorio"}, status=HTTPStatus.BAD_REQUEST)
+        
+        #validamos que no exista la categoria id
+        try:
+            categoria = Categoria.objects.get(pk=request.data.get("categoria"))
+        except Categoria.DoesNotExist:
+            return JsonResponse(
+                {"estado": "error", "mensaje": "La categoria no existe en la base de datos"},
+                status=HTTPStatus.BAD_REQUEST)
+
+        try:
+            Receta.objects.filter(pk=id).update( nombre=request.data["nombre"],
+                    slug=slugify(request.data["nombre"]),
+                    tiempo=request.data["tiempo"],
+                    descripcion=request.data["descripcion"],
+                    categoria_id=request.data["categoria"])
+            return JsonResponse({"estado": "ok", "mensaje":"Se modifica el registro existoxamente"}, status=HTTPStatus.OK)
+        except Exception as e:
+             return JsonResponse({"estado": "error", "mensaje":"Ocurrio un error inesperado"}, status=HTTPStatus.NOT_FOUND)
