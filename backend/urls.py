e23438a7bc8aa64bae82bea584df6c543e8d0f99
carolinamoralese <caromorales1396@gmail.com>
2026-03-19 17:30:08 -0500

    feat: se crea la aplicacion contacto, url y metodo post

--- backend/urls.py
+++ backend/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('', include('home.urls')),
     path('api/v1/', include('ejemplo.urls')),
     path('api/v1/', include('categorias.urls')),
-    path('api/v1/', include('recetas.urls'))
+    path('api/v1/', include('recetas.urls')),
+    path('api/v1/', include('contacto.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ contacto/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from django.http.response import HttpResponse, JsonResponse, Http404
+from http import HTTPStatus
+
+# Create your views here.
+
+class Clase1(APIView):
+
+    def post(self, request):
+        if request.data.get("nombre")== None or not request.data["nombre"]:
+            return JsonResponse({"estado": "error", "mensaje": "El campo nombre es obligatorio"}, status= HTTPStatus.BAD_REQUEST)
+        if request.data.get("correo")== None or not request.data["correo"]:
+            return JsonResponse({"estado": "error", "mensaje": "El campo correo es obligatorio"}, status= HTTPStatus.BAD_REQUEST)
+        if request.data.get("mensaje")== None or not request.data["mensaje"]:
+            return JsonResponse({"estado": "error", "mensaje": "El campo mensaje es obligatorio"}, status= HTTPStatus.BAD_REQUEST)
+        if request.data.get("telefono")== None or not request.data["telefono"]:
+            return JsonResponse({"estado": "error", "mensaje": "El campo telefono es obligatorio"}, status= HTTPStatus.BAD_REQUEST)
