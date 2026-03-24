fa16ea0443154bf0710057d2dc2b6fee6f88c5fd
carolinamoralese <caromorales1396@gmail.com>
2026-03-24 15:21:44 -0500

    feat:se crea modelo de users seguridad, se crea migracion y se ejecuta migracion

--- seguridad/models.py
+++ seguridad/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class UserMetaData(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_user} {self.last_name}"
+    
+    class Meta:
+        db_table = "users_metadata"
+        verbose_name = "User metadata"
+        verbose_name_plural = "User metadata"
 
 # Create your models here.
