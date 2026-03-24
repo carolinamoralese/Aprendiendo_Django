38336cbf9be2c93924bfc9d8c0e8fc6d06e221c4
carolinamoralese <caromorales1396@gmail.com>
2026-03-24 17:07:39 -0500

    feat:validacion de cuenta, enviando correo con token y validando ese token para activarla

--- seguridad/views.py
+++ seguridad/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from django.http.response import HttpResponse, JsonResponse, Http404
+from django.http.response import HttpResponse, JsonResponse, Http404, HttpResponseRedirect
 from http import HTTPStatus
 from .models import *
 from django.contrib.auth.models import User
 import uuid 
 import os
 from dotenv import load_dotenv
+from utilidades import utilidades
 
 class Clase1(APIView):
     
@@ -25,10 +26,38 @@ class Clase1(APIView):
         
         token = uuid.uuid4()
         url = f'{os.getenv("BASE_URL")}api/v1/seguridad/verificacion/{token}'
+
         try:
             user=User.objects.create_user(username=request.data["correo"], password=request.data["password"], email=request.data["correo"], first_name=request.data['nombre'], last_name="", is_active=0)
             UserMetaData.objects.create(token=token,user_id=user.id)
+
+            html= f"""
+            <h3>Verificacion de cuenta</h3>
+            Hola {request.data["nombre"]} te haz registrado exitosamente. Para activar tu cuenta haz click en el siguiente enlace: <br/>
+            <a href="{url}">{url}</a>
+            """
+
+            utilidades.sendMail(html, "Verificacion", request.data["correo"])
         except Exception as e:
             return JsonResponse({"estado": "error", "mensaje": "ocurrio un error inespertado"}, status=HTTPStatus.BAD_REQUEST)
         
         return JsonResponse({"estado": "ok", "mensaje": "se crea el registro exitosamente"}, status=HTTPStatus.CREATED)
+    
+
+
+class Clase2(APIView):
+    
+    def get(self, request, token):
+        if token == None or not token:
+              return JsonResponse({"estado": "error", "mensaje": "recurso no disponoble"}, status=404)
+        
+        try:
+            data=UserMetaData.objects.filter(token=token).get()
+            
+            UserMetaData.objects.filter(token=token).update(token="")
+
+            User.objects.filter(id=data.user_id).update(is_active = 1)
+
+            return HttpResponseRedirect(os.getenv("BASE_URL_FRONTEND"))
+        except UserMetaData.DoesNotExist:
+            raise Http404
